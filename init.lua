ff6f2371546ec6a6f5ddd273828b2434a04b4671
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2023-08-25 15:32:30 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,25 +2,22 @@
 
 modname = minetest.get_current_modname()
 local function transform_to_random_animal(entity)
-    local animals = {"chicken", "cow", "sheep"} -- Namen der Tiere aus der "mobs_animal" Mod
+    local animals = {
+        {texture = "mobs_kitten_black.png", mesh = "mobs_kitten.b3d"},
+        {texture = "mobs_cow.png", mesh = "mobs_cow.b3d"},
+        {texture = "mobs_pumba.png", mesh = "mobs_pumba.b3d"}
+    } -- Namen der Tiere aus der "mobs_animal" Mod
     local random_animal = animals[math.random(#animals)]
     
     entity:set_properties({
-        textures = {"mobs_animal_" .. random_animal .. ".png"},
-        visual = "mesh",
-        mesh = "player.x",
+        textures = {random_animal.texture},
+        mesh = random_animal.mesh
     })
-    
+   
     local original_pos = entity:get_pos()
     
-    minetest.after(60, function()
+    minetest.after(10, function()
         if entity:is_player() then
-            entity:set_properties({
-                textures = entity:get_properties().textures,
-                visual = entity:get_properties().visual,
-                mesh = entity:get_properties().mesh,
-            })
-            
             entity:set_pos(original_pos)  -- Zurück zur ursprünglichen Position
         end
     end)
@@ -46,7 +43,7 @@ minetest.register_tool(modname..":wabbajack_wand", {
             local distance = vector.distance(user:get_pos(), target_pos)
 
             -- Überprüfe ob das Ziel ein Spieler oder Mob ist und im Bereich von 10 Blöcken ist
-            if distance <= 10 and (target:is_player() or target:is_player()) then
+            if distance <= 10 and (target:is_player() or (target:is_player() == false and target:get_luaentity() and target:get_luaentity().name:match("mobs_animal:"))) then
                 -- Verzaubere den Spieler oder Mob für 1 Minute
                 transform_to_random_animal(target)
 
@@ -72,4 +69,3 @@ minetest.register_craft({
 })
 
 print("[MOD] Zauberstab Mod geladen")
-
